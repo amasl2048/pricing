7fdb81006cec99809f1eb9f08e12c2737d8961b4
A. Maslennikov <amasl2048@gmail.com>
2015-02-19 16:17:47 +0300

    changing

--- pricing_1a.bat
+++ pricing_1a.bat
@@ -1,5 +1,5 @@
-set FILE=new_141128.txt
-set PRICE=2014-10-30_items.csv
+set FILE=test.txt
+set PRICE=base/2015-01-30_items.csv
 pricing_1a.py %FILE% %PRICE% msrp.csv
 awk95 -f comma_1a.awk msrp.csv > msrp_co.csv
 echo "# Please add lmsrp_ru column (#8 or 'H') to msrp_co.csv"
--- pricing_3.bat
+++ pricing_3.bat
@@ -1,2 +1,2 @@
 awk95 -f dot3.awk msrp_co.csv > msrp_dot.csv
-pricing_3b.py msrp_dot.csv partners2.yml
+pricing_3b.py msrp_dot.csv base/partners3.yml
