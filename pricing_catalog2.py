2a610bff5bf0206018126dde01775d6f498e275e
A. Maslennikov <amasl2048@gmail.com>
2015-03-04 00:54:44 +0300

    new qroups disc., min buy price

--- pricing_catalog2.py
+++ pricing_catalog2.py
@@ -14,16 +14,19 @@ from numpy import loadtxt, unique
 
 print "Starting..."
 
-Conf = yaml.load(open("./base/pricing_conf.yml"))
+Conf = yaml.load(open("./base/pricing_conf2.yml"))
 
-par_file = "./base/partners3.yml"
+par_file = "./base/partners4.yml"
 Par = yaml.load(open(par_file))
 Partners = Par.keys()
 
 category_conf = yaml.load(open("./base/category_conf.yml"))
 
-group_file = category_conf["group_file"]
-groups = pd.ExcelFile(group_file).parse(category_conf["group_sheet"])
+dist = pd.ExcelFile(category_conf["dist_file"]).parse(category_conf["export"], index_col = "Part Number")
+
+si = pd.ExcelFile(category_conf["si_file"]).parse(category_conf["export"], index_col = "Part Number")
+
+groups = pd.ExcelFile(category_conf["group_file"]).parse(category_conf["group_sheet"])
 
 c = groups[["Product Group", "Material Category Name", "Disc. group"]]
 c = c.drop_duplicates()
@@ -45,79 +48,65 @@ pfile = category_conf["part_file"]
 xl1 = pd.ExcelFile(pfile)
 #print xl1.sheet_names
 part = xl1.parse(category_conf["part_sheet"])#, index_col = "partnum")
+part = part.rename(columns={"partdisc": "Product Group"})
 
 # Category file
 cat_file = category_conf["cat_file"]
 xl2 = pd.ExcelFile(cat_file)
 #print xl2.sheet_names
-category = xl2.parse(category_conf["cat_sheet"])#, index_col = "Part Number")
+category = xl2.parse(category_conf["export"])#, index_col = "Part Number")
 category = category.rename(columns={"Part Number": "partnum"})
 category = category.set_index("partnum")
 #print category.head()
 
-# Changing partcatalog name
-cat1 = loadtxt(Conf["cat1.csv"], dtype=str, usecols=(0,))
-def change_catalog1(df):
-    if (df["partnum"] in cat1): df["partcatalog"] = Conf["new1"]
-    return df
-
-part = part.apply(change_catalog1, axis = 1)
+f = lambda x: round(x, 2)
 
 def change_u(Series):
     return unicode(Series)
 
 part["partnum"] = part["partnum"].apply(change_u)
-part = part.set_index("partnum")
-
-#part["PG"] = category["Product Group"]
-part = part.rename(columns={"partdisc": "Product Group"})
-part["Material Category Name"] = category["Material Category Name"]
-part["LMSRP"] = category["Price [EUR]"]
-
-def na(Series):
-    if (Series != "NA"): return Series
-
-
-def catalog(Company, cat):
-    '''
-    Get discount value from partcatalog
-    '''
-    if (cat in Par[Company]['discount'].keys() ): return cat
-    if (cat in Conf['catalog'].keys() ): return Conf['catalog'][cat]
-    else:
-        print "ERROR1 name!", cat, Company
-        sys.exit(0)
-    return
-
-def part2group(Series):
-    '''
-    Get discont name from partcatalog
-    '''
-    if (Series in Par[Company]['discount'].keys() ): return Series
-    if (Series in Conf['catalog'].keys() ): return Conf['catalog'][Series]
-    else:
-        print "ERROR2 name!", Series
-        sys.exit(0)
-    return
-
-def disc_calc(Series):
-    return Par[Company]["discount"][catalog(Company,Series["partcatalog"])]
-
-for Company in Partners:
-    Disc = part.apply(disc_calc, axis = 1)
-    Disc = Disc[ Disc >= 0 ] # delete empty price items
-    Disc = Disc.apply(na) # delete "NA"
-    part[Company] = Disc * 100 # 100%
-#print part.head()
-
-part["Disc. group"] = part["partcatalog"].apply(part2group) # old categories discount
-
-
 
 def new_disc(Series):
     return d.loc[Series]
 
 part["New disc"] = part["Product Group"].apply(new_disc)
+part = part.set_index("partnum")
+part["old_dist_buy"] = dist["Price [USD]"]
+part["old_si_buy"] = si["Price [EUR]"]
+
+def min_price(df):
+    if (df["old_dist_buy"]/Conf["cross"] < df["old_si_buy"] ): return df["old_dist_buy"]/Conf["cross"]
+    else: return df["old_si_buy"]
+
+part["min_buy_eur"] = part.apply(min_price, axis = 1).apply(f)
+#part["new LMSRP"] = 
+part["Material Category Name"] = category["Material Category Name"]
+part["LMSRP"] = category["Price [EUR]"]
+
+def disc_calc(df):
+    new_grp = d.loc[df["Product Group"]]
+    return Par[Company]["discount"][new_grp]
+
+def na(Series):
+    if (Series != "NA"): return Series
+
+def minus(Series):
+    '''
+    buy price part = 1 - discount
+    '''
+    if (Series != "NA"): return 1 - Series
+
+for Company in Partners:
+    Disc = part.apply(disc_calc, axis = 1)
+    Disc = Disc[ Disc >= 0 ] # delete empty price items
+    Disc = Disc.apply(na) # delete "NA"
+    #part[Company] = Disc * 100 # 100%
+    if ("USD" not in Company):
+        part[Company] = part["LMSRP"] * Disc.apply(minus)
+    else:
+        part[Company] = part["LMSRP"] * Disc.apply(minus) * Conf["cross"]
+    part[Company] = part[Company].apply(f)
+
 #part.to_excel("./all_categories.xls", index=True)
 
 mcn = part["Material Category Name"].unique()
@@ -129,18 +118,16 @@ print mcn.size
 
 pgroup = part["Product Group"].unique()
 pgroup.sort()
-
-
 '''
 for row in pgroup:
     print row
 '''
-print "Product uniques groups: ", pgroup.size
+print "Product unique groups: ", pgroup.size
 
 # change index
 a = part.set_index("Product Group")
 a.sort_index(inplace=True)
-#a.to_excel("./groups6.xls", index=True)
+a.to_excel("./groups6.xls", index=True)
 
 # Unique product groups
 b = a[["partcatalog","Material Category Name"]]
