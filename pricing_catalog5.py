30345ccbbf1d194ad60f8daa3a0d2c5a29b1dc0b
A. Maslennikov <amasl2048@gmail.com>
2015-07-21 12:28:13 +0200

    add commets, disc. groups

--- pricing_catalog5.py
+++ pricing_catalog5.py
@@ -1,44 +1,40 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import yaml
+import pandas as pd
+import sys
 '''
 Combine Product Group, Material Category Name and Disc.
 Check koef. k and calc new k_new & buy_p
 
-2015 June
+2015 July
 '''
-import yaml
-import pandas as pd
-import sys
-
 print "Starting..."
 
-Par = yaml.load(open("./base/partners5.yml"))
-Partners = Par.keys()
-
+# load config
 category_conf = yaml.load(open("./base/category_conf.yml"))
 cross = category_conf["cross"]
 
-dist = pd.ExcelFile(category_conf["dist_file"]).parse(category_conf["export"], index_col = "Part Number")
+# load partners data
+Par = yaml.load(open("./base/partners5.yml"))
+Partners = Par.keys()
 
+# load prices
+dist = pd.ExcelFile(category_conf["dist_file"]).parse(category_conf["export"], index_col = "Part Number")
 si = pd.ExcelFile(category_conf["si_file"]).parse(category_conf["export"], index_col = "Part Number")
 
-def mba(Series):
-    s = u"MBA"
-    if (Series in category_conf["new_cat"]): return s
-    return Series
-
-# Read prepared product groups discounts
-d = pd.ExcelFile(category_conf["prod_groups"]).parse("Sheet1")
+# Read prepared discounts (Disc. group) per product group
+d = pd.ExcelFile(category_conf["prod_groups"]).parse("Sheet1") # groups_unique
 d = d.set_index("Product Group")
 d = d["Disc. group"]
 
+# Read all part.numbers, product groups, partrefp
+# 'part_file' need to be prepared from items.csv filtered from actual LMSRP partnum
 part = pd.ExcelFile(category_conf["part_file"]).parse(category_conf["part_sheet"])
 part = part.rename(columns={"partdisc": "Product Group"})
 
-# Category file
-cat_file = category_conf["cat_file"]
-xl2 = pd.ExcelFile(cat_file)
-category = xl2.parse(category_conf["export"])#, index_col = "Part Number")
+# Read category file (extract from LMSRP price)
+category = pd.ExcelFile(category_conf["cat_file"]).parse(category_conf["export"])#, index_col = "Part Number")
 category = category.rename(columns={"Part Number": "partnum"})
 category = category.set_index("partnum")
 #print category.head()
@@ -55,13 +51,13 @@ def new_disc(Series):
     get new discount group from Product Group: "RC" -> "MBA"
     In : d.loc[part["Product Group"][0]]
     Out: u'MBA'
-    !!! should be no empty items !!!
     '''
-    if pd.isnull(part["Product Group"]).any():
+    if pd.isnull(part["Product Group"]).any(): # should be no empty items!
         print "Error: empty product group..."
         sys.exit(0)
     return d.loc[Series]
 
+# adding new calculating columns
 part["New disc"] = part["Product Group"].apply(new_disc)
 part = part.set_index("partnum")
 part["old_dist_buy"] = dist["Price [USD]"]
@@ -119,6 +115,7 @@ def new_lmsrp(df):
     else: 
         return df["buy_new"] / (1 - category_conf["max_disc"])
 
+# adding new calculating columns
 part["min_buy_eur"] = part.apply(min_price, axis = 1)#.apply(f)
 part["k"] = part.apply(k_ref, axis = 1).apply(f)
 part["k_new"] = part.apply(k_new, axis = 1).apply(f)
@@ -140,6 +137,7 @@ def minus(Series):
     '''
     if (Series != "NA"): return 1 - Series
 
+# adding new calculating columns "Company"
 P_disc = pd.DataFrame() # partners discount per product group
 for Company in Partners:
     Disc = part.apply(disc_calc, axis = 1)
@@ -152,18 +150,15 @@ for Company in Partners:
         part[Company] = part["new LMSRP"] * Disc.apply(minus) * cross
     part[Company] = part[Company].apply(f)
 
-
+# calculation difference new-old
 part["diff_si_buy"] = part[category_conf["gold_si"]] - part["old_si_buy"]
 part["diff_dist_buy"] = part[category_conf["gold_dist"]] - part["old_dist_buy"]
-
 #part.to_excel("./all_categories.xls", index=True)
 
 mcn = part["Material Category Name"].unique()
-
 for each in mcn:
     print each
 print "\nMaterial Category Names: ", mcn.size
-
 
 pgroup = part["Product Group"].unique()
 pgroup.sort()
@@ -174,12 +169,15 @@ for row in pgroup:
 print "Items: ", part.index.size
 print "Product groups: ", pgroup.size
 
-# Change index
-b = part[["Product Group", "Material Category Name"]]
+# Preparing product discount per product group per partner 
+b = part[["Product Group", "Material Category Name", "New disc"]]
 for Company in Partners:
     b[Company] = P_disc[Company]
+
+# Change index
 part.reset_index(inplace = True)
 
+# Whole sorted output from part DataFrame
 #a = part.set_index("Product Group")
 a = part.set_index("partnum")
 a.sort_index(inplace=True)
@@ -188,11 +186,12 @@ try:
 except:
     print "\nError: 'buy_new.xls' is busy..."
 
-lmsrp_ru = part[["partnum", "partlabel", "partmsrp", "partrefp", "partxferbasep","Product Group"]]
+# New lmsrp_ru
+lmsrp_ru = part[["partnum", "partlabel", "partmsrp", "partrefp", "partxferbasep", "Product Group"]]
 lmsrp_ru = lmsrp_ru.rename(columns={"Product Group": "partdisc"})
 lmsrp_ru["lmsrp_ru"] = part["new LMSRP"]
 lmsrp_ru.reset_index(inplace = True)
-lmsrp_ru.to_excel("./msrp_ru.xls", index=False)
+lmsrp_ru.to_excel("./lmsrp_ru.xls", index=False)
 
 # Unique product groups
 b.set_index("Product Group", inplace = True)
@@ -201,7 +200,7 @@ b.reset_index(inplace = True)
 #print b.head()
 b = b.drop_duplicates()
 b.to_excel("./partners_discounts.xls", index=False)
-del b["Material Category Name"]
+del b["Material Category Name"] # only unique remain
 b = b.drop_duplicates()
 b.to_excel("./partners_discounts_uniq.xls", index=False)
 
